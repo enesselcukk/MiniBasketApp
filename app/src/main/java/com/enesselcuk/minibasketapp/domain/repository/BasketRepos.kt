f4474f2c77471ce60036a58811483a25bd55a676
enes <enesselcu25@gmail.com>
2022-07-19 00:29:45 +0300

    created BasketRepos.kt and BasketRepositoryImpl.kt

--- app/src/main/java/com/enesselcuk/minibasketapp/domain/repository/BasketRepos.kt
+++ app/src/main/java/com/enesselcuk/minibasketapp/domain/repository/BasketRepos.kt
@@ -1,10 +1,10 @@
-package com.enesselcuk.minibasketapp.repository
+package com.enesselcuk.minibasketapp.domain.repository
 
 import com.enesselcuk.minibasketapp.remote.model.BasketResponse
 import com.enesselcuk.minibasketapp.util.NetworkResult
 import kotlinx.coroutines.flow.Flow
 
 interface BasketRepos {
-    suspend fun getProduct(): Flow<NetworkResult<BasketResponse>>
-
+    suspend fun getProduct(
+    ): Flow<NetworkResult<BasketResponse>>
 }
--- app/src/main/java/com/enesselcuk/minibasketapp/remote/repos/BasketRepositoryImpl.kt
+++ app/src/main/java/com/enesselcuk/minibasketapp/remote/repos/BasketRepositoryImpl.kt
@@ -1,11 +1,32 @@
 package com.enesselcuk.minibasketapp.remote.repos
 
 
+import com.enesselcuk.minibasketapp.remote.model.BasketResponse
 import com.enesselcuk.minibasketapp.remote.service.BasketService
+import com.enesselcuk.minibasketapp.domain.repository.BasketRepos
+import com.enesselcuk.minibasketapp.util.NetworkResult
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 class BasketRepositoryImpl @Inject constructor(
     private val api: BasketService,
-) {
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+
+) : BasketRepos {
+    override suspend fun getProduct(
+
+    ): Flow<NetworkResult<BasketResponse>> =
+        flow {
+            emit(NetworkResult.Loading())
+            try {
+                val data = api.getStories()
+                emit(NetworkResult.Success(data))
+            } catch (ex: Throwable) {
+                emit(NetworkResult.Error(ex.localizedMessage))
+            }
+        }.flowOn(dispatcher)
+
 
 }
