e410d01e64db8e4985dcb412082f704d63f3193a
enes <enesselcu25@gmail.com>
2022-07-19 00:30:12 +0300

    created BasketService.kt

--- app/src/main/java/com/enesselcuk/minibasketapp/remote/service/BasketService.kt
+++ app/src/main/java/com/enesselcuk/minibasketapp/remote/service/BasketService.kt
@@ -4,12 +4,10 @@ import com.enesselcuk.minibasketapp.remote.model.BasketResponse
 import retrofit2.http.GET
 
 
-
 // https://hepsiemlak.keen4e.repl.co/listing
 interface BasketService {
-    @GET("stores")
+    @GET("listing")
     suspend fun getStories(): BasketResponse
-
 
 
 
